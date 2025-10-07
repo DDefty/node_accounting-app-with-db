16a6fb0d7d8c7a83989df3a62cf71b135ff4a924
DDefty <dawid.kogut99@gmail.com>
2025-10-07 13:47:43 +0200

    Refactor Category model definition for improved readability

--- src/models/Category.model.js
+++ src/models/Category.model.js
@@ -3,20 +3,24 @@
 const DataTypes = require('sequelize');
 const { sequelize } = require('../db.js');
 
-const Category = sequelize.define('Category', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+const Category = sequelize.define(
+  'Category',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
+  {
+    timestamps: false,
   },
-}, {
-  timestamps: false,
-});
+);
 
 module.exports = {
   Category,
